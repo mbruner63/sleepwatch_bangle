3236a6fe9216e0fee5fd7d2ec64d9b0d8737830a
Martin Bruner <marty@bruner-consulting.com>
2022-09-16 11:01:45 -0600

    Font change on clock

--- sleepwatch_bangle.js
+++ sleepwatch_bangle.js
@@ -752,28 +752,28 @@ function stepsCheckForReset(){
 }
 
 var draw = () => {
-  // work out how to display the current time
-  var d = new Date();
-  var h = d.getHours(), m = d.getMinutes();
-  var time = (" "+h).substr(-2) + ":" + ("0"+m).substr(-2);
-  // Reset the state of the graphics library
-  g.reset();
-  // draw the current time (4x size 7 segment)
-  g.setFont("7x11Numeric7Seg",4);
-  g.setFontAlign(1,1); // align right bottom
-  g.drawString(time, X, Y, true /*clear background*/);
-  // draw the seconds (2x size 7 segment)
-  g.setFont("7x11Numeric7Seg",2);
-  g.drawString(("0"+d.getSeconds()).substr(-2), X+30, Y, true /*clear background*/);
-  // draw the date, in a normal font
-  g.setFont("6x8");
-  g.setFontAlign(0,0); // align center bottom
-  // pad the date - this clears the background if the date were to change length
-  var dateStr = "    "+require("locale").date(d)+"    ";
-  g.drawString(dateStr, g.getWidth()/2, Y+15, true /*clear background*/);
-  g.setFont("Vector12");
-  g.drawString(Version,g.getWidth()/2, g.getHeight-15,false);
-};
+    // work out how to display the current time
+    var d = new Date();
+    var h = d.getHours(), m = d.getMinutes();
+    var time = (" "+h).substr(-2) + ":" + ("0"+m).substr(-2);
+    // Reset the state of the graphics library
+    g.reset();
+    // draw the current time (4x size 7 segment)
+    g.setFont("7x11Numeric7Seg",4);
+    g.setFontAlign(1,1); // align right bottom
+    g.drawString(time, X, Y, true /*clear background*/);
+    // draw the seconds (2x size 7 segment)
+    g.setFont("7x11Numeric7Seg",2);
+    g.drawString(("0"+d.getSeconds()).substr(-2), X+30, Y, true /*clear background*/);
+    // draw the date, in a normal font
+    g.setFont("6x8");
+    g.setFontAlign(0,0); // align center bottom
+    // pad the date - this clears the background if the date were to change length
+    var dateStr = "    "+require("locale").date(d)+"    ";
+    g.drawString(dateStr, g.getWidth()/2, Y+15, true /*clear background*/);
+    g.setFont("Vector14");
+    g.drawString(Version,g.getWidth()/2, Y+50 ,false);
+  };
    
 function clockDisplay_enable(){
   secondInterval = setInterval(draw, 1000);
